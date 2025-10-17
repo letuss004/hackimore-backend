68e7a40a09cb77a9be1ec8cc4d7f81746cc2357b
Le Anh Tu <tu.le3@sotatek.com>
2025-10-17 10:12:37 +0700

    configure S3Service with AWS region and credentials from environment variables

--- src/integration/aws/s3.service.ts
+++ src/integration/aws/s3.service.ts
@@ -27,7 +27,13 @@ import { ServerLogger } from '@server/logger';
 @Injectable()
 export class S3Service extends S3Client {
   constructor() {
-    super({});
+    super({
+      region: process.env.AWS_REGION,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
+    });
   }
 
   async uploadObject(params: PutObjectCommandInput) {
