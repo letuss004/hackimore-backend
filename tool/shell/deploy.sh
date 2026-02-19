d5d810aa5be95ec7262ec656dbc84f4087424bef
Le Anh Tu <tu.le3@sotatek.com>
2026-02-20 01:39:49 +0700

    chore(deploy): simplify deployment script

--- tool/shell/deploy.sh
+++ tool/shell/deploy.sh
@@ -3,28 +3,6 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Check if sshpass is installed
-if ! command -v sshpass &> /dev/null; then
-  echo "sshpass could not be found. Please install sshpass to proceed."
-  exit 1
-fi
-
-# Default values
-DEFAULT_SERVER_ADDRESS="52.221.220.151"
-DEFAULT_USERNAME="ubuntu"
-
-# Read inputs from the user with default values
-read -p "Enter server address [${DEFAULT_SERVER_ADDRESS}]: " serverAddress
-serverAddress=${serverAddress:-$DEFAULT_SERVER_ADDRESS}
-
-read -p "Enter username [${DEFAULT_USERNAME}]: " username
-username=${username:-$DEFAULT_USERNAME}
-
+git pull
 yarn build
-rsync -avz \
-    -e "ssh -i aws-access-key.pem" \
-    --exclude 'node_modules' \
-    --exclude '.env' \
-    --exclude '.git' \
-    ./ \
-    "$username"@"$serverAddress":/home/ubuntu/apps/hackimore-backend
+pm2 start --attach
