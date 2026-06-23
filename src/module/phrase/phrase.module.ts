01941402a541f71aaa027e36110b3a03c3dff80f
trampol1nes <letuss004@gmail.com>
2026-06-23 20:38:15 +0700

    feat/PhraseAudioRating

--- src/module/phrase/phrase.module.ts
+++ src/module/phrase/phrase.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { PhraseAudioService } from 'src/module/phrase/audio/phrase-audio.service';
-import { PhraseAudioRatingController } from 'src/module/phrase/rating/phrase-audio-rating.controller';
-import { PhraseAudioRatingService } from 'src/module/phrase/rating/phrase-audio-rating.service';
 import { PhraseRetryController } from 'src/module/phrase/retry/phrase-retry.controller';
 import { PhraseRetryService } from 'src/module/phrase/retry/phrase-retry.service';
 import { PhraseAudioController } from './audio/phrase-audio.controller';
@@ -10,18 +8,8 @@ import { PhraseService } from './phrase.service';
 
 @Module({
   imports: [],
-  controllers: [
-    PhraseController,
-    PhraseAudioController,
-    PhraseRetryController,
-    PhraseAudioRatingController,
-  ],
-  providers: [
-    PhraseService,
-    PhraseAudioService,
-    PhraseRetryService,
-    PhraseAudioRatingService,
-  ],
+  controllers: [PhraseController, PhraseAudioController, PhraseRetryController],
+  providers: [PhraseService, PhraseAudioService, PhraseRetryService],
   exports: [],
 })
 export class PhraseModule {}
